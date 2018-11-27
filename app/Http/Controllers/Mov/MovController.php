4b799c1ff3d96ebb69d8070e4596ea2ff5d8ff49
jconde01 <jconde1@gmail.com>
2018-11-27 17:39:40 -0600

    body

--- app/Http/Controllers/Mov/MovController.php
+++ app/Http/Controllers/Mov/MovController.php
@@ -17,6 +17,7 @@ class MovController extends Controller
 
     public const FACTURA = 'VTA.FAC';
     public const REMISION = 'VTA.REM';
+    public const VENTA_WEB = 3;
     /**
      * Display a listing of the resource.
      *
@@ -53,16 +54,21 @@ class MovController extends Controller
             $movH->folio = $folio->Folio_Sig;
             $movH->estatus = 'Concluido';
             $movH->Origen_Modulo = "shopif";
-            $movH->Origen_Mov = $request->input('id_compra');
-            $movH->Descuentos = $request->input('descuento_total');
-            //$movH->desc_porc = $request->input('descuento_porcentaje_total'); // Manejaremos descuento por renglon
-            $email = explode('@', $request->comprador["email"]);
+            $movH->Origen_Mov = $request->body['id_compra'];
+            $movH->Descuentos = $request->body['descuento_total'];
+            //$movH->desc_porc = $request->input->body('descuento_porcentaje_total'); // Manejaremos descuento por renglon
+            $email = explode('@', $request->body["comprador"]["email"]);
             $movH->entregado_por = $email[0];
             $movH->transportado_por = $email[1];
-            $movH->Importe_Total = $request->input('importe_total');
+            $movH->Importe_Total = $request->body['importe_total'];
             $movH->cliente_id = 1;
+            if ($request->body['forma_pago'] == 1) {
+                $movH->c_FormaPago = "01";              // Efectivo (OXXO)
+            } else {
+                $movH->c_FormaPago = "04";              // Tarjeta de crédito, Paypal
+            }            
             $movH->save();
-            foreach ($request->productos as $key => $product) {
+            foreach ($request->body["productos"] as $key => $product) {
                  $movD = New MovDetail();
                  $movD->idmov_h = $movH->idmov_h;
                  $itemID = Item::where('item_sku',$product['sku'])->select('item_id')->first();  // !!!!!! try .... catch ???
@@ -87,7 +93,7 @@ class MovController extends Controller
         $movH = new MovHeader();
         DB::transaction(function () use($request,$movH) {
 
-            $id_compra = $request->input('id_compra');
+            $id_compra = $request->body['id_compra'];
             // Localiza la compra entre las remisiones guardadas
             // para obtener la información para generar la factura
             try {
@@ -99,15 +105,15 @@ class MovController extends Controller
             
             // Busca un cliente con el RFC pasado. Si no existe lo agrega a la BD
             try {
-                $cliente = Client::where('RFC',$request->input('rfc'))->firstOrFail();
+                $cliente = Client::where('RFC',$request->body['rfc'])->firstOrFail();
                 $cliCon = CliCon::find($cliente->cliente_id);    
             } catch ( Illuminate\Database\Eloquent\ModelNotFoundException $e ) {
                 // Agrega el cliente a la BD
                 $cliente = New Client();
-                $cliente->Nombre = $request->input('razon_social');
-                $cliente->RFC = $request->input('rfc');
-                $cliente->CP = $request->input('CP');
-                $cliente->c_UsoCFDI = $request->input('cfdi');
+                $cliente->Nombre = $request->body['razon_social'];
+                $cliente->RFC = $request->body['rfc'];
+                $cliente->CP = $request->body['CP'];
+                $cliente->c_UsoCFDI = $request->body['cfdi'];
                 $cliente->save();
                 $cliCon = New CliCon();
                 $cliCon->NOMCON = 'contacto 1';
@@ -124,17 +130,18 @@ class MovController extends Controller
             $movH->estatus = 'Concluido';
             $movH->referencia = "";
             $movH->Notas = "Venta por Internet";
+            $movH->Origen = $this::VENTA_WEB;
             $movH->Origen_Modulo = "shopif";
-            $movH->Origen_Mov = $request->input('id_compra');
+            $movH->Origen_Mov = $request->body['id_compra'];
             $movH->Descuentos = $compra->Descuentos;
             //$movH->desc_porc = $request->input('descuento_porcentaje_total'); // Manejaremos descuento por renglon
             $movH->Importe_Total = $compra->Importe_Total;
             $movH->cliente_id = $cliente->cliente_id;
             $movH->SinExistencias = 2;              // Factura CLASE B
             $movH->Remision = $compra->idmov_h;     // Remisión asociada
-            $movH->c_UsoCFDI = $request->input('cfdi');
+            $movH->c_UsoCFDI = $request->body['cfdi'];
             $movH->c_MetodoPago = "PUE";
-            $movH->c_FormaPago = "04";              // Tarjeta de crédito
+            $movH->c_FormaPago = $compra->c_FormaPago;
             $movH->save();
             foreach ($detail as $key => $product) {
                  $movD = New MovDetail();
